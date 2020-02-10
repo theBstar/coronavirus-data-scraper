33b6421fb40f61f0f4f299b7cae5289d6c3f0841
bikram <6bikramsutar@gmail.com>
2020-02-10 21:10:53 +0530

    moved js to src dir

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const httpRequest = require('request-promise');
+
+const { PORT } = require('./config');
+const { scrapeDataFromHTML } = require('./scraper');
+const InMemoryCache = require('./InMemoryCache');
+const { makeSuccessfullResponse, isDataExpired } = require('./utils');
+const {
+  DATA_URL,
+  ERROR_RESPONSE,
+} = require('./constants');
+
+const app = express();
+
+function buildSuccessResponder(response) {
+  return function sendSuccessResponse(data) {
+    const result = makeSuccessfullResponse(data);
+    response.status(200).json(result);
+  }
+}
+
+
+app.get('/', async (request, response) => {
+  const sendSuccessResponse = buildSuccessResponder(response);
+  const resultFromCache = InMemoryCache.getData();
+  if (resultFromCache && !isDataExpired(resultFromCache)) {
+    sendSuccessResponse(resultFromCache);
+    return;
+  }
+
+  httpRequest(DATA_URL)
+    .then(function (html) {
+      const resultSet = scrapeDataFromHTML(html);
+      if (!resultSet) throw Error('Could not scrape the data');
+      InMemoryCache.setData(resultSet);
+      sendSuccessResponse(InMemoryCache.getData());
+      return;
+    })
+    .catch(function (e) {
+      const resultFromCache = InMemoryCache.getData();
+      if (resultFromCache) {
+        sendSuccessResponse(resultFromCache);
+        return;
+      }
+      response.status(500).json(ERROR_RESPONSE);
+      return;
+    });
+});
+
+module.exports = function () {
+  app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`);
+  });
+}
